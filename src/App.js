a469a95aa2c59815ea4bd08de57843841218214d
sofiamollon <smollon@udesa.edu.ar>
2021-03-23 12:42:49 -0300

    React y creado componente Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+// IMPORTO LOS ARCHIVOS DE LA CARPETA COMPONENTS
+import Header from "./components/Header"
+
 function App() {
   return (
-      <h1>Hola</h1>
+    <div className="App">
+      <Header/>
+    </div>
   );
 }
 
