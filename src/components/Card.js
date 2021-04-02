3993d26b0db18769e7cf8a8fbdde76308a161c1c
miliscipioni <scipionimilagros@gmail.com>
2021-04-02 09:24:11 -0300

    complete el componente Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,13 @@ import React from 'react'
             <img src={props.elemento.picture} alt="" className='w-full' />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">
-                    <p>{props.elemento.name.last}</p>
+                    <h2>{props.elemento.name.last}</h2> 
+                    <h2>{props.elemento.name.first}</h2>
+                    <h3> {props.elemento.email}</h3>
+                    <h4> {props.elemento.dob.date} - ({props.elemento.dob.age})</h4>
+
+                    
+
                 </div>
             </div>            
         </div>
